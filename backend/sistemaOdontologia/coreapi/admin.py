a7437e7b5bb986459c042bd4e2e583873586df2b
Isacc Mealla <isacc.mealla.psn@gmail.com>
2025-12-11 10:31:13 -0400

    avance de historial

--- backend/sistemaOdontologia/coreapi/admin.py
+++ backend/sistemaOdontologia/coreapi/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Pacientes, HistorialesClinicos, ContactosEmergencia, Usuarios, Roles
+from .models import Pacientes, HistorialesClinicos, ContactosEmergencia, Usuarios, Roles, RegistroHistoriaClinica
 
 
 @admin.register(Pacientes)
@@ -30,6 +30,34 @@ class UsuariosAdmin(admin.ModelAdmin):
 class RolesAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre')
     search_fields = ('nombre',)
-from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(RegistroHistoriaClinica)
+class RegistroHistoriaClinicaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'paciente', 'estudiante', 'materia', 'tipo_registro', 'estado', 'fecha_registro')
+    list_filter = ('materia', 'tipo_registro', 'estado', 'is_deleted', 'fecha_registro')
+    search_fields = ('paciente__nombres', 'paciente__apellidos', 'estudiante__username', 'estudiante__nombres')
+    date_hierarchy = 'fecha_registro'
+    readonly_fields = ('fecha_registro', 'fecha_modificacion', 'fecha_aprobacion')
+    
+    fieldsets = (
+        ('Información Básica', {
+            'fields': ('id', 'historial', 'estudiante', 'paciente')
+        }),
+        ('Categorización', {
+            'fields': ('materia', 'tipo_registro', 'estado')
+        }),
+        ('Datos del Registro', {
+            'fields': ('datos',)
+        }),
+        ('Aprobación', {
+            'fields': ('aprobado_por', 'fecha_aprobacion', 'observaciones_docente')
+        }),
+        ('Auditoría', {
+            'fields': ('fecha_registro', 'fecha_modificacion', 'is_deleted', 'deleted_at', 'deleted_by'),
+            'classes': ('collapse',)
+        }),
+    )
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('paciente', 'estudiante', 'aprobado_por')
